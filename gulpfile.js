4658f02742fa7506dd82fd6184ff14bbbd0a0178
Yevgeniy Korpussenko <e.korpousenko@gmail.com>
2021-10-13 22:51:49 +0200

    add gulp (final)

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const { task } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 // Static server
 gulp.task('server', function() {
@@ -15,6 +18,14 @@ gulp.task('server', function() {
 gulp.task('styles', function() {
     return gulp.src("src/sass/*.+(scss|sass)")
         .pipe (sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({
+            prefix: "",
+            suffix: ".min",
+          }))
+          .pipe(autoprefixer({
+			cascade: false
+		}))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe (gulp.dest("src/css"))
         .pipe (browserSync.stream());
 });
