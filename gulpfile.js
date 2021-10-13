02d4e969ee84f3bb1018a3c645d773d196b5fb88
Yevgeniy Korpussenko <e.korpousenko@gmail.com>
2021-10-13 22:08:58 +0200

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const { task } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+// Static server
+gulp.task('server', function() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/sass/*.+(scss|sass)")
+        .pipe (sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe (gulp.dest("src/css"))
+        .pipe (browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+    gulp.watch("src/*.html").on("change", browserSync.reload);
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
